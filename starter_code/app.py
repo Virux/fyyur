cbf93652200b484c33c080aec685fda8fbe5836c
Virux <starletgh@gmail.com>
2022-08-16 22:11:04 +0000

    Completed code for Artist listing

--- starter_code/app.py
+++ starter_code/app.py
@@ -243,6 +243,10 @@ def show_venue(venue_id):
 @app.route('/venues/create', methods=['GET'])
 def create_venue_form():
   form = VenueForm()
+  #if form.is_validate():
+    #artist = Post(
+      #name=form.name.data,
+      #)
   return render_template('forms/new_venue.html', form=form)
 
 @app.route('/venues/create', methods=['POST'])
@@ -440,12 +444,52 @@ def create_artist_form():
 
 @app.route('/artists/create', methods=['POST'])
 def create_artist_submission():
-  # called upon submitting the new artist listing form
-  # TODO: insert form data as a new Venue record in the db, instead
-  # TODO: modify data to be the data object returned from db insertion
+  error = False
+  try:
+    name = request.form['name']
+    city = request.form['city']
+    state = request.form['state']
+    phone = request.form['phone']
+    genres = request.form['genres']
+    image_link = request.form['image_link']
+    facebook_link = request.form['facebook_link']
+    website_link = request.form['website_link']
+    seeking_venue = request.form['seeking_venue']
+    if seeking_venue == "y":
+      seeking_venue = True
+    else:
+      seeking_venue = False
+    seeking_description = request.form['seeking_description']
 
-  # on successful db insert, flash success
-  flash('Artist ' + request.form['name'] + ' was successfully listed!')
+    artist = Artist(
+      name = name,
+      city = city,
+      state = state,
+      phone = phone,
+      genres = genres,
+      image_link = image_link,
+      facebook_link = facebook_link,
+      website_link = website_link,
+      seeking_venue = seeking_venue,
+      seeking_description = seeking_description
+    )
+
+    # called upon submitting the new artist listing form
+    # TODO: insert form data as a new Artist record in the db, instead
+    db.session.add(artist)
+    db.session.commit()
+  except:
+    error = True
+    db.session.rollback()
+  finally:
+    db.session.close()
+  if error:
+    flash('An error occurred. Artist ' + request.form['name'] + ' could not be listed.')
+  else:
+    #if not error:
+    # TODO: modify data to be the data object returned from db insertion
+    # on successful db insert, flash success
+    flash('Artist ' + request.form['name'] + ' was successfully listed!')
   # TODO: on unsuccessful db insert, flash an error instead.
   # e.g., flash('An error occurred. Artist ' + data.name + ' could not be listed.')
   return render_template('pages/home.html')
